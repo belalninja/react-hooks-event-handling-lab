71b1b192cd58809d2102c4ae8339d14f5b4bbdd0
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-10-22 16:51:34 +0300

    Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const logPass = () => console.log("Entering password...");
+  return (
+    <div>
+      <input type="password" name="Nuclear keypad" onChange={logPass} />
+    </div>
+  );
 }
 
 export default Keypad;
